f6c42fa03d7859a8b58bacf3039fcbd0fa775578
Denis Krumko <dkrumko@gmail.com>
2018-06-14 17:50:03 +0300

    Fix English Russian layout

--- django_keyboard_layout/layouts.py
+++ django_keyboard_layout/layouts.py
@@ -7,7 +7,7 @@ __all__ = ('DefaultLayout', 'EnglishRussianLayout')
 class BaseLayout(object):
     """Base class for keyboard layout."""
 
-    layout_dict = None
+    layout = None
 
     @classmethod
     def create(cls, source_low, source_up, dest_low, dest_up):
@@ -17,25 +17,48 @@ class BaseLayout(object):
         search term to transform it to different layout.
 
         """
-        result_dict = {}
+        source_dict = {}
         source = source_low + source_up
+
+        dest_dict = {}
         dest = dest_low + dest_up
 
         for index, char in enumerate(source):
-            result_dict[char] = dest[index]
+            source_dict[char] = dest[index]
 
         for index, char in enumerate(dest):
-            result_dict[char] = source[index]
+            dest_dict[char] = source[index]
 
-        return result_dict
+        return {
+            'source_dict': source_dict,
+            'source_set': set(source),
+            'dest_dict': dest_dict,
+            'dest_set': set(dest),
+        }
 
     @classmethod
     def convert_term(cls, term):
         """Method to convert search term to current layout."""
-        if not cls.layout_dict:
+        if not cls.layout:
             return term
 
-        return reduce(lambda a, b: a + cls.layout_dict.get(b, b), term, '')
+        layout_dict = cls.detect_layout_dict(term)
+
+        return reduce(lambda a, b: a + layout_dict.get(b, b), term, '')
+
+    @classmethod
+    def detect_layout_dict(cls, term):
+        """Method to detect required layout (source or destination).
+
+        Method searches for larger intersections between set of term chars
+        and sets of source and destination layouts. Larger intersection
+        defines used layout.
+
+        """
+        source_len = len(set(term) & cls.layout['source_set'])
+        dest_len = len(set(term) & cls.layout['dest_set'])
+        name = 'source_dict' if source_len > dest_len else 'dest_dict'
+        return cls.layout[name]
 
 
 class DefaultLayout(BaseLayout):
@@ -49,14 +72,10 @@ class DefaultLayout(BaseLayout):
 
 class EnglishRussianLayout(BaseLayout):
     """English - Russian keyboard layout."""
-    english_lowercase = "qwertyuiop[]asdfghjkl;'zxcvbnm,./`"
-    english_uppercase = "QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?~"
-    russian_lowercase = "йцукенгшщзхъфывапролджэячсмитьбю.ё"
-    russian_uppercase = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё"
 
-    layout_dict = BaseLayout.create(
-        source_low=english_lowercase,
-        source_up=english_uppercase,
-        dest_low=russian_lowercase,
-        dest_up=russian_uppercase
+    layout = BaseLayout.create(
+        "qwertyuiop[]asdfghjkl;'zxcvbnm,./`",
+        "QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?~",
+        "йцукенгшщзхъфывапролджэячсмитьбю.ё",
+        "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё"
     )
